ff1248f8a3f87a4a7178aeb74f0a17e44a1a57e2
Arrel <darrelrabbani4@.com>
2025-02-21 16:28:20 +0700

    fixing bug img

--- src/About/Populer.jsx
+++ src/About/Populer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Image3 from '../assets/image3.png';
-import Image2 from '../assets/image2.png';
-import Image5 from '../assets/image5.png';
+import Image3 from '../assets/Image3.png';
+import Image2 from '../assets/Image2.png';
+import Image5 from '../assets/Image5.png';
 import { motion } from "framer-motion";
 
 export const SlideUp = (delay) => {
